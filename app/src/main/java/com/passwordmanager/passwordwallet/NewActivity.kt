5f0d6163ceffff8fc8a45fa1aef4352c94281ffa
sreekanth-k-work <emailtosreekanthk@gmail.com>
2024-06-21 23:18:57 +0530

    Add Details screen revamped

--- app/src/main/java/com/passwordmanager/passwordwallet/NewActivity.kt
+++ app/src/main/java/com/passwordmanager/passwordwallet/NewActivity.kt
@@ -1,20 +1,41 @@
 package com.example.passwordmanager
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.passwordmanager.passwordwallet.R
 
-
 class NewActivity : AppCompatActivity() {
+    private var websiteEditText: EditText? = null
+    private var usernameEditText: EditText? = null
+    private var passwordEditText: EditText? = null
+    private var submitButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.passwordmanager.passwordwallet.R.layout.activity_new)
+        setContentView(R.layout.activity_new)
 
-    }
+        websiteEditText     = findViewById<EditText>(R.id.id_website)
+        usernameEditText    = findViewById<EditText>(R.id.id_username)
+        passwordEditText    = findViewById<EditText>(R.id.id_password)
+        submitButton        = findViewById<Button>(R.id.id_submit_button)
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        overridePendingTransition(com.passwordmanager.passwordwallet.R.anim.no_animation, com.passwordmanager.passwordwallet.R.anim.slide_down)
+        submitButton?.setOnClickListener(View.OnClickListener {
+            val website     = websiteEditText!!.getText().toString().trim { it <= ' ' }
+            val username    = usernameEditText!!.getText().toString().trim { it <= ' ' }
+            val password    = passwordEditText!!.getText().toString().trim { it <= ' ' }
+            if (website.isEmpty() || username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this@NewActivity, "All fields are required", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // Handle the submission, e.g., save to database or pass back to previous activity
+                Toast.makeText(this@NewActivity, "Submitted!", Toast.LENGTH_SHORT).show()
+                // For now, just finish the activity
+                finish()
+            }
+        })
     }
 }
