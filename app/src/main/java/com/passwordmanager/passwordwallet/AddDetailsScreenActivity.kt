79b235202f5bb3337e3631e9a2ea4b53ae6245cf
sreekanth-k-work <emailtosreekanthk@gmail.com>
2024-06-22 02:18:16 +0530

    toggle switch for password added

--- app/src/main/java/com/passwordmanager/passwordwallet/AddDetailsScreenActivity.kt
+++ app/src/main/java/com/passwordmanager/passwordwallet/AddDetailsScreenActivity.kt
@@ -1,27 +1,41 @@
 package com.example.passwordmanager
 
+import android.R
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.passwordmanager.passwordwallet.R
+
 
 class AddDetailsScreenActivity : AppCompatActivity() {
     private var websiteEditText: EditText? = null
     private var usernameEditText: EditText? = null
     private var passwordEditText: EditText? = null
     private var submitButton: Button? = null
+    private var showPwdToggle:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_details_screen_layout)
+        setContentView(com.passwordmanager.passwordwallet.R.layout.add_details_screen_layout)
 
-        websiteEditText     = findViewById<EditText>(R.id.id_website)
-        usernameEditText    = findViewById<EditText>(R.id.id_username)
-        passwordEditText    = findViewById<EditText>(R.id.id_password)
-        submitButton        = findViewById<Button>(R.id.id_submit_button)
+        websiteEditText     = findViewById<EditText>(com.passwordmanager.passwordwallet.R.id.id_website)
+        usernameEditText    = findViewById<EditText>(com.passwordmanager.passwordwallet.R.id.id_username)
+        passwordEditText    = findViewById<EditText>(com.passwordmanager.passwordwallet.R.id.id_password)
+        submitButton        = findViewById<Button>(com.passwordmanager.passwordwallet.R.id.id_submit_button)
+
+        showPwdToggle       = findViewById(com.passwordmanager.passwordwallet.R.id.showPasswordToggle)
+        showPwdToggle?.setOnClickListener(View.OnClickListener {
+            // Toggle password visibility
+            if (passwordEditText!!.getInputType() == (InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+                passwordEditText!!.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)
+            } else {
+                passwordEditText!!.setInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
+            }
+        })
+
 
         submitButton?.setOnClickListener(View.OnClickListener {
             val website     = websiteEditText!!.getText().toString().trim { it <= ' ' }
@@ -42,6 +56,6 @@ class AddDetailsScreenActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
 
-        overridePendingTransition(R.anim.stay, R.anim.slide_out_down);
+//        overridePendingTransition(R.anim.stay, R.anim.slide_out_down);
     }
 }
